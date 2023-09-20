2eb3d01197ef0f28e1c93d40b24cdb12c5cc1c22
Kunal Patankar <99674202+KunalPatankar2002@users.noreply.github.com>
2023-09-20 15:50:11 +0530

    Time: 2 ms (9.18%), Space: 41.9 MB (78.45%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int weight=0, len=nums.length,min=0,minLoc=0;
+        // for(int i=0;i<len;i++){
+        //     weight=nums[len-1-i]*Math.pow(10,i);
+        // }
+        for(int i=len-2;i>-1;i--){
+            if(nums[i]<nums[i+1]){
+                minLoc=i+1;
+                for(int j=i+1; j<len; j++){
+                    if(nums[j] > nums[i] && nums[j] < nums[minLoc]){
+                        minLoc=j;
+                        System.out.println(minLoc);
+                    }
+                }
+                int temp = nums[i];
+                nums[i] = nums[minLoc];
+                nums[minLoc] = temp;
+                
+                Arrays.sort(nums,i+1,len);
+                break;
+            }
+            else if(i==0){
+                Arrays.sort(nums);
+                break;
+            }
+        }
+    }
+}
